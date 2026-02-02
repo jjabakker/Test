1d09aa3c3dec8698c4bac84ee9d4e9e71760a928
Hans Bakker <jjabakker@icloud.com>
2026-02-02 19:14:10 +0100

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Hans Bakker on 02/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
