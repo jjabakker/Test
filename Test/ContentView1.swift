e4d24c88b75aa641ccf931d7688dbd589f0730d9
Hans Bakker <jjabakker@icloud.com>
2026-02-03 20:06:56 +0100

    Two ContebntViewa

---
+++ Test/ContentView1.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Hans Bakker on 02/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("IMG_7716")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+        
+        
+        /*
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .padding(1)
+            Text("Hello, hello, hello, hello, world!")
+                .border(Color.blue)
+                .padding(1)
+            Text("New Test")
+                .border(Color.red)
+                .padding(1)
+            Text("New Test")
+                .border(Color.red)
+        }
+        .padding()
+        */
+    }
+}
+
+#Preview {
+    ContentView()
+}
